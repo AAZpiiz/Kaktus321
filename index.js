568f170b2794bb319fd4ecf1c302dc3994d5b2f1
AAZpiiz <60482902+AAZpiiz@users.noreply.github.com>
2020-03-26 21:38:02 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const PREFIX = '?';
+
+bot.on('ready', () =>{
+  console.log('Botti käynnistetty');
+  bot.user.setActivity('KaktusCord', { type: 'STREAMING'}).catch(console.error);
+})
+
+bot.on('message', message=>{
+
+  let args = message.content.substring(PREFIX.length).split(" ");
+  
+  switch(args[0]){
+    case 'moi':
+    
+    message.reply(`moi`);
+
+    break;
+
+})
+
+bot.login(process.env.BOT_TOKEN);
